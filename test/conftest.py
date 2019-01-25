426b4751d7b10f6034cc183ec6b1bf529448fb27
Jakob van Santen <jvansanten@gmail.com>
2019-01-25 15:03:19 +0100

    Expose fixtures for use in other packages

--- test/conftest.py
+++ test/conftest.py
@@ -4,6 +4,8 @@ import pytest
 
 from ampel.test.fixtures import docker_service
 
+pytest_plugins = ['ampel.ztf.test.fixtures']
+
 @pytest.fixture(scope="session")
 def kafka():
 	gen = docker_service('spotify/kafka', 9092,
@@ -24,37 +26,6 @@ def kafka_stream(kafka, alert_tarball):
 		producer.produce('ztf_20180819_programid1', fileobj.read())
 	producer.flush()
 	yield kafka
-
-@pytest.fixture(scope='session')
-def alert_tarball():
-	return join(dirname(__file__), '..', 'alerts', 'ztf_public_20180819_mod1000.tar.gz')
-
-@pytest.fixture(scope='session')
-def alert_generator(alert_tarball):
-	import itertools
-	import fastavro
-	from ampel.pipeline.t0.load.TarballWalker import TarballWalker
-	def alerts(with_schema=False):
-		atat = TarballWalker(alert_tarball)
-		for fileobj in itertools.islice(atat.get_files(), 0, 1000, 1):
-			reader = fastavro.reader(fileobj)
-			alert = next(reader)
-			if with_schema:
-				yield alert, reader.schema
-			else:
-				yield alert
-	return alerts
-
-@pytest.fixture(scope='session')
-def lightcurve_generator(alert_generator):
-	from ampel.utils.ZIAlertUtils import ZIAlertUtils
-	def lightcurves():
-		for alert in alert_generator():
-			lightcurve = ZIAlertUtils.to_lightcurve(content=alert)
-			assert isinstance(lightcurve.get_photopoints(), tuple)
-			yield lightcurve
-
-	return lightcurves
 
 @pytest.fixture(scope='session')
 def transientview_generator(alert_generator):
