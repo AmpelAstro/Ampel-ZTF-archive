16f85f76d3cff275745388dacac1a4fddd5091f5
Jakob van Santen <jvansanten@gmail.com>
2021-03-11 14:46:06 +0100

    test: remove unused fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,3 @@ from os import environ
 import pytest
 
 pytest_plugins = ["tests.fixtures"]
-
-
-@pytest.fixture
-def latest_schema():
-    with open(
-        join(dirname(__file__), "..", "..", "..", "alerts", "schema_2.0.json")
-    ) as f:
-        return json.load(f)
