ca5e517fb94a93caf32c38e14130e2dd1ecac9a0
Jakob van Santen <jvansanten@gmail.com>
2021-06-02 09:24:36 +0200

    Validate alert in chunks

--- ampel/ztf/archive/server/models.py
+++ ampel/ztf/archive/server/models.py
@@ -1,64 +1,76 @@
-
 from typing import List, Dict, Any, Literal, Optional
 from pydantic import BaseModel, Field
 
-
-class AlertChunk(BaseModel):
-    resume_token: str
-    chunks_remaining: int
-    alerts: List[Dict[str, Any]]
 
 class StreamDescription(BaseModel):
     resume_token: str
     chunk_size: int
     chunks: int
 
+
 class Topic(BaseModel):
     description: str = Field(..., description="Informative string for this topic")
-    candids: List[int] = Field(..., description="IPAC candidate ids to associate with this topic")
+    candids: List[int] = Field(
+        ..., description="IPAC candidate ids to associate with this topic"
+    )
+
 
 class TopicDescription(BaseModel):
     topic: str
     description: str = Field(..., description="Informative string for this topic")
     size: int
 
+
 class TopicQuery(BaseModel):
     topic: str
     chunk_size: int = Field(
-        100,  gte=100, lte=10000, description="Number of alerts per chunk"
+        100, gte=100, lte=10000, description="Number of alerts per chunk"
     )
     start: Optional[int] = Field(None, gte=0)
     stop: Optional[int] = Field(None, gte=1)
     step: Optional[int] = Field(None, gt=0)
 
+
 class ConeConstraint(BaseModel):
-    ra: float = Field(..., description="Right ascension of field center in degrees (J2000)")
-    dec: float = Field(..., description="Declination of field center in degrees (J2000)")
-    radius: float = Field(..., gt=0, lt=180, description="Radius of search cone in degrees")
+    ra: float = Field(
+        ..., description="Right ascension of field center in degrees (J2000)"
+    )
+    dec: float = Field(
+        ..., description="Declination of field center in degrees (J2000)"
+    )
+    radius: float = Field(
+        ..., gt=0, lt=180, description="Radius of search cone in degrees"
+    )
+
 
 class TimeConstraint(BaseModel):
     lt: Optional[float] = Field(None)
     gt: Optional[float] = Field(None)
+
 
 class AlertQuery(BaseModel):
     cone: Optional[ConeConstraint] = None
     jd: TimeConstraint = TimeConstraint()
     programid: Optional[int] = None
     chunk_size: int = Field(
-        100,  gte=0, lte=10000, description="Number of alerts per chunk"
+        100, gte=0, lte=10000, description="Number of alerts per chunk"
     )
+
 
 class AlertCutouts(BaseModel):
     """
     Images are gzipped FITS files, b64 encoded
     """
+
     template: str
     science: str
     difference: str
 
+
 # Generated from tests/test-data/schema_3.3.json
 # 1. Convert avro to json-schema with https://json-schema-validator.herokuapp.com/avro.jsp
 # 2. Convert json-schema to pydantic with datamodel-codegen --input schema_3.3.json --output alert --use-schema-description
+
 
 class Candidate(BaseModel):
     """
@@ -71,7 +83,7 @@ class Candidate(BaseModel):
     diffmaglim: Optional[float]
     pdiffimfilename: Optional[str]
     programpi: Optional[str]
-    programid: Literal[1,2,3]
+    programid: Literal[1, 2, 3]
     candid: int
     isdiffpos: str
     tblid: int
@@ -242,6 +254,7 @@ class Cutout(BaseModel):
     fileName: str
     stampData: str
 
+
 class Alert(BaseModel):
     """
     avro alert schema for ZTF (www.ztf.caltech.edu)
@@ -256,3 +269,9 @@ class Alert(BaseModel):
     cutoutScience: Optional[Cutout]
     cutoutTemplate: Optional[Cutout]
     cutoutDifference: Optional[Cutout]
+
+
+class AlertChunk(BaseModel):
+    resume_token: str
+    chunks_remaining: int
+    alerts: List[Alert]
