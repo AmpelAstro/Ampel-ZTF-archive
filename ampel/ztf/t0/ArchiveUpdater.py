67f7af997531653809361cc49bda4b5b293ac5e5
Jakob van Santen <jvansanten@gmail.com>
2022-02-14 15:50:44 +0100

    Accept alerts with empty history

--- ampel/ztf/t0/ArchiveUpdater.py
+++ ampel/ztf/t0/ArchiveUpdater.py
@@ -140,7 +140,7 @@ class ArchiveUpdater(ArchiveDBClient):
         # come with unique ids) and upper limits (which don't)
         detections = []
         upper_limits = []
-        for index, c in enumerate(alert["prv_candidates"]):
+        for index, c in enumerate(alert["prv_candidates"] or []):
             # entries with no candid are nondetections
             if c["candid"] is None:
                 upper_limits.append(c)
