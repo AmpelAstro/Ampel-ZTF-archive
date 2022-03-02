c7f7761a9182768db8bbd2a93e9cd4d06988c156
Jakob van Santen <jvansanten@gmail.com>
2022-03-02 19:54:07 +0100

    Disable cron for tests

--- tests/test-data/initdb/archive/21_read_queue_ack.sql
+++ tests/test-data/initdb/archive/21_read_queue_ack.sql
@@ -3,6 +3,6 @@ ALTER TABLE
 ADD
     issued TIMESTAMP WITH TIME ZONE;
 
-SELECT cron.schedule('*/5 * * * *',$$
-UPDATE read_queue SET issued = NULL WHERE now() - issued > interval '10 minutes';
-$$);
+-- SELECT cron.schedule('*/5 * * * *',$$
+-- UPDATE read_queue SET issued = NULL WHERE now() - issued > interval '10 minutes';
+-- $$);
